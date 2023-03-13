2bc3618a9a3c6cd02a9ee25f590e1aba1f2c5995
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 15:51:13 -0400

    category create complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,7 +15,6 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
     where: {
       id: req.params.id
@@ -28,6 +27,9 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((dbCategoryData) => res.status(200).json(dbCategoryData))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.put('/:id', (req, res) => {
