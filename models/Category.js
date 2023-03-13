e7e9b8b13254dd2fc07504639e2b1ace2df19cc5
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 16:42:27 -0400

    category model created

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: { // This is the primary key
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: { // This is the name of the category
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
