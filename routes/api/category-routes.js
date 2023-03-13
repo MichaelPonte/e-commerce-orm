85931c6c597ff66887f1bd438ef5e827d7b4d6c5
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 15:44:40 -0400

    findOne route complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,6 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
   Category.findAll({
     include: [Product]
   })
@@ -17,6 +16,14 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product],
+  })
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
