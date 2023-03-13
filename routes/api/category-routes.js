a45117065bf396d4d800bcd4cc47d2bdfffbd39e
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 15:55:31 -0400

    category delete complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  
+  Category.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((dbCategoryData) => res.status(200).json(dbCategoryData))
+  .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
