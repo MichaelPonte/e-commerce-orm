d9fc4c2c7c6391154e6cf6e9b3ec86f98a5eaa2a
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 16:43:13 -0400

    productTag model created

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: { // This is the primary key
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: { // This is the foreign key, it grabs the id from the product model
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: { // This is the id of the tag
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
