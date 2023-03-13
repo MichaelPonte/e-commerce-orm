d46a2bbdf2d91ff17c4c107d72c13820e6e3f451
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 15:53:50 -0400

    category update complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,6 +34,13 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbCategoryData) => res.status(200).json(dbCategoryData))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
