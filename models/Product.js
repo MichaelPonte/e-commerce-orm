6644608fdfdbb1caf6fefd3173507cdb60dab769
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 16:42:38 -0400

    product model created

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: { // This is the primary key
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: { // This is the name of the product
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: { // This is the price of the product
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: { // This is the stock of the product
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: { // This is the foreign key, it grabs the id from the category model
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
