582a9c010ba043050146b09fcf37bb492ffecff2
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 16:14:48 -0400

    tag findAll complete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,12 +5,22 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      }
+    ]
+  })
+  .then((dbTagData) => res.json(dbTagData))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  
 });
 
 router.post('/', (req, res) => {
