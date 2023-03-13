121a18c2033055d3162dc3620fa89f4deeb864d6
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 16:45:17 -0400

    added sequelize

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection'); // This is the connection to the database
 
 const app = express();
 const PORT = process.env.PORT || 3001;
