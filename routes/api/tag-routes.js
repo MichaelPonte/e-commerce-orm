93d678c0acb2a1cc94d580a299807ada1a45c0c9
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 16:23:26 -0400

    tag delete route complete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -54,6 +54,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbTagData) => res.status(200).json(dbTagData))
+  .catch((err) => res.status(404).json(err));
 });
 
 module.exports = router;
