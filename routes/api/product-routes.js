4e1033a1d6b0ff0788b094c0d652d0cf49782b2d
Michael Ponte <michaelponte02@hotmail.com>
2023-03-13 16:10:59 -0400

    product routes complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      }
+    ]
+  })
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => res.status(500).json(err));
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      }
+    ]
+  })
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => res.status(500).json(err));
 });
 
 // create new product
@@ -91,6 +114,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((dbProductData) => res.status(200).json(dbProductData))
+  .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
